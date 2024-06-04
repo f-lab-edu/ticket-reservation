13b43ca71e1febc09fc4b549fd176646aed5fbf1
EunJung Yoo <warmth424@gmail.com>
2024-06-04 21:10:15 +0900

    fix: showRequest에 validation annotation 추가

--- ticketing-core/src/main/java/com/ticketing/solution/application/ShowFacade.java
+++ ticketing-core/src/main/java/com/ticketing/solution/application/ShowFacade.java
@@ -3,7 +3,7 @@ package com.ticketing.solution.application;
 import com.ticketing.solution.domain.show.Show;
 
 public interface ShowFacade {
-    void updateShow(Long showId, Show mapToShow, Long eventId, Long hallId);
+    void updateShow(Long showId, Show show, Long eventId, Long hallId);
 
     void addShow(Show mapToShow, Long eventId, Long hallId);
 }
