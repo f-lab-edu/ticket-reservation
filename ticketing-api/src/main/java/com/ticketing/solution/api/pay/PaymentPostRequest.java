59ad7ee9d7a4ca001f33f8b85ce9d8e91335e6bb
EunJung Yoo <warmth424@gmail.com>
2024-06-06 15:52:23 +0900

    fix: payment dto에 validation 기능 추가

--- ticketing-api/src/main/java/com/ticketing/solution/api/pay/PaymentPostRequest.java
+++ ticketing-api/src/main/java/com/ticketing/solution/api/pay/PaymentPostRequest.java
@@ -1,12 +1,24 @@
 package com.ticketing.solution.api.pay;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Builder;
+import org.hibernate.validator.constraints.Length;
 
 @Builder
 public record PaymentPostRequest(
+        @NotBlank
+        @Length(min = 1, max = 255)
         String imp_uid,
+
+        @NotBlank
+        @Length(min = 1, max = 255)
         String status,
+
+        @Length(max = 255)
         String cancellation_id,
+
+        @NotBlank
+        @Length(min = 1, max = 255)
         String name
 ) {
 }
--- ticketing-api/src/main/java/com/ticketing/solution/api/pay/PaymentPreRequest.java
+++ ticketing-api/src/main/java/com/ticketing/solution/api/pay/PaymentPreRequest.java
@@ -1,13 +1,25 @@
 package com.ticketing.solution.api.pay;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.Builder;
+import org.hibernate.validator.constraints.Length;
 
 import java.math.BigDecimal;
 
 @Builder
 public record PaymentPreRequest(
-        String merchant_uid,
+
+        @NotBlank
+        @Length(max = 255)
+        String merchantUid,
+
+        @NotNull
         Long showId,
+
+        @NotNull
+        @Positive
         BigDecimal amount
 ) {
 }
